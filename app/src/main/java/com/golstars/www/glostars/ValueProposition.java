1320e66c5ea71a07556563cb5270333277634cc3
Shanto16 <shakil.glostars@gmail.com>
2017-01-31 04:18:45 +0600

    Initial commit

---
+++ app/src/main/java/com/golstars/www/glostars/ValueProposition.java
@@ -0,0 +1,32 @@
+package com.golstars.www.glostars;
+
+import android.support.v4.app.Fragment;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * Created by admin on 1/31/2017.
+ */
+
+public class ValueProposition extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.value_proposition_activity, container, false);
+
+        return rootView;
+    }
+}
