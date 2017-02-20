d75ccce480459aa5218dfffd21ac7b72a800a22b
Shanto16 <shakil.glostars@gmail.com>
2017-02-20 21:50:10 +0600

    competition user

--- app/src/main/java/com/golstars/www/glostars/competitionAll.java
+++ app/src/main/java/com/golstars/www/glostars/competitionAll.java
@@ -1,162 +1,126 @@
 package com.golstars.www.glostars;
 
-import android.support.design.widget.TabLayout;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 public class competitionAll extends AppCompatActivity {
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    private SectionsPagerAdapter mSectionsPagerAdapter;
 
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    private ViewPager mViewPager;
+    Button gallerybut;
+    Button tipsbut;
+    Button termsbut;
+    Button recogbut;
+
+
+
+    GridView gallery;
+    LinearLayout tips;
+    ScrollView terms;
+    ScrollView recog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_competition_all);
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
+        gallery = (GridView)findViewById(R.id.gallerygrid);
+        tips = (LinearLayout)findViewById(R.id.tipslin);
+        terms = (ScrollView)findViewById(R.id.termslin);
+        recog = (ScrollView)findViewById(R.id.recoglin);
 
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(mViewPager);
+        gallerybut = (Button)findViewById(R.id.gallerybut);
+        tipsbut = (Button)findViewById(R.id.tipsbut);
+        termsbut = (Button)findViewById(R.id.termsbut);
+        recogbut = (Button)findViewById(R.id.recogbut);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+
+        //==========================================================================
+
+        gallerybut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                gallerybut.setTextColor(getResources().getColor(R.color.colorPrimary));
+                tipsbut.setTextColor(getResources().getColor(R.color.darkGrey));
+                termsbut.setTextColor(getResources().getColor(R.color.darkGrey));
+                recogbut.setTextColor(getResources().getColor(R.color.darkGrey));
+
+
+                gallery.setVisibility(View.VISIBLE);
+                tips.setVisibility(View.GONE);
+                terms.setVisibility(View.GONE);
+                recog.setVisibility(View.GONE);
+
+            }
+        });
+
+
+        termsbut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                termsbut.setTextColor(getResources().getColor(R.color.colorPrimary));
+                tipsbut.setTextColor(getResources().getColor(R.color.darkGrey));
+                gallerybut.setTextColor(getResources().getColor(R.color.darkGrey));
+                recogbut.setTextColor(getResources().getColor(R.color.darkGrey));
+
+
+                terms.setVisibility(View.VISIBLE);
+                tips.setVisibility(View.GONE);
+                gallery.setVisibility(View.GONE);
+                recog.setVisibility(View.GONE);
+            }
+        });
+
+
+
+        tipsbut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tipsbut.setTextColor(getResources().getColor(R.color.colorPrimary));
+                gallerybut.setTextColor(getResources().getColor(R.color.darkGrey));
+                termsbut.setTextColor(getResources().getColor(R.color.darkGrey));
+                recogbut.setTextColor(getResources().getColor(R.color.darkGrey));
+
+
+                tips.setVisibility(View.VISIBLE);
+                gallery.setVisibility(View.GONE);
+                terms.setVisibility(View.GONE);
+                recog.setVisibility(View.GONE);
+            }
+        });
+
+
+        recogbut.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                recogbut.setTextColor(getResources().getColor(R.color.colorPrimary));
+                tipsbut.setTextColor(getResources().getColor(R.color.darkGrey));
+                termsbut.setTextColor(getResources().getColor(R.color.darkGrey));
+                gallerybut.setTextColor(getResources().getColor(R.color.darkGrey));
+
+
+                recog.setVisibility(View.VISIBLE);
+                tips.setVisibility(View.GONE);
+                terms.setVisibility(View.GONE);
+                gallery.setVisibility(View.GONE);
             }
         });
 
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_competition_all, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_competition_posts, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "SECTION 1";
-                case 1:
-                    return "SECTION 2";
-                case 2:
-                    return "SECTION 3";
-            }
-            return null;
-        }
-    }
 }
--- app/src/main/java/com/golstars/www/glostars/edit_profile.java
+++ app/src/main/java/com/golstars/www/glostars/edit_profile.java
@@ -6,8 +6,19 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Locale;
 
 public class edit_profile extends AppCompatActivity {
+
+    Spinner currentcountry;
+    Spinner homecountry;
+    Spinner occupation;
+    ArrayAdapter<CharSequence> occupationadapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +27,30 @@ public class edit_profile extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
+        currentcountry = (Spinner) findViewById(R.id.currentcountryspinner);
+        homecountry = (Spinner) findViewById(R.id.homecountryspinner);
+        occupation = (Spinner)findViewById(R.id.occupationspinner);
 
+        occupationadapter = ArrayAdapter.createFromResource(this,R.array.occupation,android.R.layout.simple_spinner_item);
+        occupationadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        occupation.setAdapter(occupationadapter);
+
+
+        Locale[] locale = Locale.getAvailableLocales();
+        ArrayList<String> countries = new ArrayList<>();
+        String country;
+        for( Locale loc : locale ){
+            country = loc.getDisplayCountry();
+            if( country.length() > 0 && !countries.contains(country) ){
+                countries.add( country );
+            }
+        }
+        Collections.sort(countries, String.CASE_INSENSITIVE_ORDER);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item, countries);
+        homecountry.setAdapter(adapter);
+        currentcountry.setAdapter(adapter);
+
+    }
 }
