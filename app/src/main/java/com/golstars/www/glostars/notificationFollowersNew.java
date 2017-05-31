4f759e8752130ee00a46e4868235f9bef8749891
Shanto16 <shakil.glostars@gmail.com>
2017-06-01 03:09:35 +0600

    check it

--- app/src/main/java/com/golstars/www/glostars/notificationFollowersNew.java
+++ app/src/main/java/com/golstars/www/glostars/notificationFollowersNew.java
@@ -3,16 +3,23 @@ package com.golstars.www.glostars;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class notificationFollowersNew extends Fragment {
 
+
+    RecyclerView followerNew;
+
     @Override
     public View onCreateView(LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.activity_notification_followers_new, container, false);
+
+
+        followerNew = (RecyclerView)rootView.findViewById(R.id.followerRecyclerNew);
 
         return rootView;
     }
--- app/src/main/java/com/golstars/www/glostars/notificationNotificationNew.java
+++ app/src/main/java/com/golstars/www/glostars/notificationNotificationNew.java
@@ -3,16 +3,21 @@ package com.golstars.www.glostars;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class notificationNotificationNew extends Fragment {
 
+    RecyclerView notificationNew;
+
     @Override
     public View onCreateView(LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
         final View rootView = inflater.inflate(R.layout.activity_notification_notification_new, container, false);
+
+        notificationNew = (RecyclerView)rootView.findViewById(R.id.notificationRecyclerNew);
 
         return rootView;
     }
